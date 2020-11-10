ef2e93c5043500ef097b37c3854ffef9ec7a0785
GabrielaNarvaezA <74216933+GabrielaNarvaezA@users.noreply.github.com>
2020-11-10 18:52:12 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,16 +1,16 @@
 
 #SEMESTRE III
-Mediciones_electromagneticas=subject(3,[],['Fund_fisica_experimental'])
+Mediciones_electromagneticas=subject(3,[],['Fundamentos_de_fisica_experimental'])
 Electricidad_y_magnetismo=subject(3,[],['Calculo_integral', 'Calculo_vectorial'])
-Programacion=subject(3,[],['Alegbra_lineal'])
-Probabilidad=subject(3,['Calculo_diferencial'])
+Optativa_programacion_y_metodos_numericos=subject(3,[],['Algebra_lineal'])
+Optativa_estadistica_basica=subject(3,['Calculo_diferencial'])
 Calculo_de_ecuaciones_diferenciales_ordinarias=subject(4,['Calculo_integral'])
 
 #SEMESTRE VI
 Optativa=subject(3,[])
 Mecanica_analitica_I=subject(3,['Mecanica_newtoniana'])
 Oscilaciones_y_ondas=subject(3,['Mecanica_newtoniana'])
-Matematicas_especiales_I=subject(3,['Calculo_de_ecuaciones_diferenciales_ordinarias'])
+Matematicas_especiales_I_para_fisica=subject(3,['Calculo_de_ecuaciones_diferenciales_ordinarias'])
 Relatividad=subject(3,['Electricidad_y_magnetismo'])
 
 #SEMESTRE V
