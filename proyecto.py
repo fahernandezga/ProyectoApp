cd1bf8f6904319e8738acb6f44b87cc0236465d2
GabrielaNarvaezA <74216933+GabrielaNarvaezA@users.noreply.github.com>
2020-11-10 18:28:46 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,19 +1,14 @@
-# -*- coding: utf-8 -*-
-print("Hola mundotz")
-ghghh
-a=1
 
+#SEMESTRE III
+Mediciones_electromagneticas=subject(3,[],[Fund_fisica_experimental])
+Electricidad_y_magnetismo=subject(3,[],[Calculo_integral, Calculo_vectorial])
+Programacion=subject(3,[],[Alegbra_lineal])
+Probabilidad=subject(3,[Calculo_diferencial])
+Calculo_de_ecuaciones_diferenciales_ordinarias=subject(4,[Calculo_integral])
 
-#Quinto semestre
-Experimentos_de_física_moderna=subject(3,['Fundamentos de física experimental'])
-Mecánica_analítica_II=subject(3,['Mecánica Analítica I'])
-Electrodinámica_I=subject(3,['Matemáticas especiales I para física'])
-Matemáticas_especiales_II_para_física=subject(3,['Cálculo de ecuaciones diferenciales ordinarias','Matemáticas especiales I para física'])
-Optativa_Electrónica_e_instrumentación=subject(3,['Mediciones electromagnéticas'])
-
-#Sexto semestre
-Termodinámica_módulo_experimental=subject(2,['Mediciones electromagnéticas'])
-Termodinámica_módulo_de_teoría=subject(2,['Electricidad y magnetismo'])
-Electrodinámica_II=subject(3,['Electrodinámica I'])
-Mecánica_Cuántica_I=subject(4,['Mecánica analítica I','Matemáticas especiales I para física','Experimentos de física moderna'])
-Optativa_Herramientas matemáticas_y_computacionales=subject(3,['Optativa - Programación y métodos numéricos','Matemáticas especiales I para física'])
+#SEMESTRE VI
+Optativa=subject(3,[])
+Mecanica_analitica_I=subject(3,[Mecanica_newtoniana])
+Oscilaciones_y_ondas=subject(3,[Mecanica_newtoniana])
+Matematicas_especiales_I=subject(3,[Calculo_de_ecuaciones_diferenciales_ordinarias])
+Relatividad=subject(3,[Electricidad_y_magnetismo])
