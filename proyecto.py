aac5f044188738e175ca0efbe81696602efb6d07
fahernandezga <73623780+fahernandezga@users.noreply.github.com>
2020-11-10 18:34:39 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -14,15 +14,15 @@ Matematicas_especiales_I=subject(3,['Calculo_de_ecuaciones_diferenciales_ordinar
 Relatividad=subject(3,['Electricidad_y_magnetismo'])
 
 #SEMESTRE V
-Experimentos_de_física_moderna=subject(3,['Fundamentos de física experimental'])
-Mecánica_analítica_II=subject(3,['Mecánica Analítica I'])
-Electrodinámica_I=subject(3,['Matemáticas especiales I para física'])
-Matemáticas_especiales_II_para_física=subject(3,['Cálculo de ecuaciones diferenciales ordinarias','Matemáticas especiales I para física'])
-Optativa_Electrónica_e_instrumentación=subject(3,['Mediciones electromagnéticas'])
+Experimentos_de_fisica_moderna=subject(3,['Fundamentos_de_fisica_experimental'])
+Mecanica_analitica_II=subject(3,['Mecanica_Analitica I'])
+Electrodinamica_I=subject(3,['Matematicas_especiales_I_para_fisica'])
+Matematicas_especiales_II_para_fisica=subject(3,['Calculo_de_ecuaciones_diferenciales_ordinarias','Matematicas_especiales_I_para_fisica'])
+Optativa_Electronica_e_instrumentacion=subject(3,['Mediciones_electromagneticas'])
 
 #SEMESTRE VI
-Termodinámica_módulo_experimental=subject(2,['Mediciones electromagnéticas'])
-Termodinámica_módulo_de_teoría=subject(2,['Electricidad y magnetismo'])
-Electrodinámica_II=subject(3,['Electrodinámica I'])
-Mecánica_Cuántica_I=subject(4,['Mecánica analítica I','Matemáticas especiales I para física','Experimentos de física moderna'])
-Optativa_Herramientas matemáticas_y_computacionales=subject(3,['Optativa - Programación y métodos numéricos','Matemáticas especiales I para física'])
+Termodinamica_modulo_experimental=subject(2,['Mediciones_electromagneticas'])
+Termodinamica_modulo_de_teoria=subject(2,['Electricidad_y_magnetismo'])
+Electrodinamica_II=subject(3,['Electrodinamica_I'])
+Mecanica_Cuantica_I=subject(4,['Mecanica_analitica_I','Matematicas_especiales_I_para_fisica','Experimentos_de_fisica_moderna'])
+Optativa_Herramientas_matematicas_y_computacionales=subject(3,['Optativa_Programacion_y_metodos_numericos','Matematicas_especiales_I_para_fisica'])
