42b1e65872cbd89fac494894ef47e8552094383b
fahernandezga <73623780+fahernandezga@users.noreply.github.com>
2020-11-10 18:29:28 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -12,3 +12,17 @@ Mecanica_analitica_I=subject(3,[Mecanica_newtoniana])
 Oscilaciones_y_ondas=subject(3,[Mecanica_newtoniana])
 Matematicas_especiales_I=subject(3,[Calculo_de_ecuaciones_diferenciales_ordinarias])
 Relatividad=subject(3,[Electricidad_y_magnetismo])
+
+#SEMESTRE V
+Experimentos_de_física_moderna=subject(3,['Fundamentos de física experimental'])
+Mecánica_analítica_II=subject(3,['Mecánica Analítica I'])
+Electrodinámica_I=subject(3,['Matemáticas especiales I para física'])
+Matemáticas_especiales_II_para_física=subject(3,['Cálculo de ecuaciones diferenciales ordinarias','Matemáticas especiales I para física'])
+Optativa_Electrónica_e_instrumentación=subject(3,['Mediciones electromagnéticas'])
+
+#SEMESTRE VI
+Termodinámica_módulo_experimental=subject(2,['Mediciones electromagnéticas'])
+Termodinámica_módulo_de_teoría=subject(2,['Electricidad y magnetismo'])
+Electrodinámica_II=subject(3,['Electrodinámica I'])
+Mecánica_Cuántica_I=subject(4,['Mecánica analítica I','Matemáticas especiales I para física','Experimentos de física moderna'])
+Optativa_Herramientas matemáticas_y_computacionales=subject(3,['Optativa - Programación y métodos numéricos','Matemáticas especiales I para física'])
