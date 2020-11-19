9737b8889974c4e07166ffc3e1d5629b64c75c2a
GabrielaNarvaezA <74216933+GabrielaNarvaezA@users.noreply.github.com>
2020-11-18 22:15:50 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -18,6 +18,8 @@ from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
  
 Builder.load_string("""
 <MenuScreen>:
